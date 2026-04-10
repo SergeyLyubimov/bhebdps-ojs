204fcf06868f7cf5440eebc781ce2ab967d5d530
Black_m1n <modnyak0001@mail.ru>
2026-04-10 20:39:31 +1000

    Homework 8.1

---
+++ Домашние задания/3.4 Анимация загрузки данных/task.js
@@ -0,0 +1,21 @@
+async function getData() {
+  const response = await fetch('https://students.netoservices.ru/nestjs-backend/slow-get-courses');
+  const body = await response.json();
+
+  document.getElementById('items').innerHTML = Object.values(body.response.Valute).map(item => 
+    `<div class="item">
+      <div class="item__code">
+      ${item.CharCode}
+      </div>
+      <div class="item__value">
+      ${item.Value}
+      </div>
+      <div class="item__currency">
+      руб.
+      </div>
+    </div>`).join('');
+
+  document.getElementById('loader').classList.remove('loader_active');
+}
+
+getData();
